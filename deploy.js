108283dfa37e42286ab8557cd0105728cffbe651
4rMyy <56570563+starmkodinw@users.noreply.github.com>
2024-03-22 21:48:05 +0700

    Fixed solc

--- deploy.js
+++ deploy.js
@@ -18,17 +18,19 @@ async function main() {
     //   process.env.PRIVATE_KEY_PASSWORD
     // );
     // wallet = wallet.connect(provider);
-    const abi = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.abi", "utf8")
+    console.log(provider, process.env.RPC_URL);
+    console.log(wallet);
+    const abi = fs.readFileSync("./SimpleContract_sol_SimpleContract.abi", "utf8")
     const binary = fs.readFileSync(
-        "./SimpleStorage_sol_SimpleStorage.bin",
+        "./SimpleContract_sol_SimpleContract.bin",
         "utf8"
     )
     const contractFactory = new ethers.ContractFactory(abi, binary, wallet)
     console.log("Deploying, please wait...")
     const contract = await contractFactory.deploy()
     // const contract = await contractFactory.deploy({ gasPrice: 100000000000 })
-    const deploymentReceipt = await contract.deployTransaction.wait(1)
-    console.log(`Contract deployed to ${contract.address}`)
+    // const deploymentReceipt = await contract.deployTransaction.wait(1)
+    console.log(contract)
     // console.log("Here is the transaction:")
     // console.log(contract.deployTransaction)
     // console.log("Here is the receipt:")
@@ -59,13 +61,13 @@ async function main() {
     // const sentTxResponse = await wallet.sendTransaction(tx);
     // console.log(resp)
 
-    let currentFavoriteNumber = await contract.retrieve()
-    console.log(`Current Favorite Number: ${currentFavoriteNumber}`)
-    console.log("Updating favorite number...")
-    let transactionResponse = await contract.store(7)
-    let transactionReceipt = await transactionResponse.wait()
-    currentFavoriteNumber = await contract.retrieve()
-    console.log(`New Favorite Number: ${currentFavoriteNumber}`)
+    // let currentFavoriteNumber = await contract.retrieve()
+    // console.log(`Current Favorite Number: ${currentFavoriteNumber}`)
+    // console.log("Updating favorite number...")
+    // let transactionResponse = await contract.store(7)
+    // let transactionReceipt = await transactionResponse.wait()
+    // currentFavoriteNumber = await contract.retrieve()
+    // console.log(`New Favorite Number: ${currentFavoriteNumber}`)
 }
 
 main()
